fe62a87833d4f81e5ec6fa690f539a5476128276
QueenaHuang <cindy119xyz@gmail.com>
2018-05-17 15:18:28 +1000

    Initial commit

---
+++ TheWeather/AppDelegate.swift
@@ -0,0 +1,35 @@
+//
+//  AppDelegate.swift
+//  TheWeather
+//
+//  Created by QueenaHuang on 17/5/18.
+//  Copyright © 2018 queenahu. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+
+    func applicationWillResignActive(_ application: UIApplication) {
+    }
+
+    func applicationDidEnterBackground(_ application: UIApplication) {
+    }
+
+    func applicationWillEnterForeground(_ application: UIApplication) {
+    }
+
+    func applicationDidBecomeActive(_ application: UIApplication) {
+    }
+
+    func applicationWillTerminate(_ application: UIApplication) {
+        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    }
+
+}
---
+++ TheWeather/MainTableViewController.swift
@@ -0,0 +1,42 @@
+//
+//  MainTableViewController.swift
+//  TheWeather
+//
+//  Created by QueenaHuang on 17/5/18.
+//  Copyright © 2018 queenahu. All rights reserved.
+//
+
+import UIKit
+
+class MainTableViewController: UITableViewController {
+    private let reuseIdentifier = "CellId"
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 4
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath) as? UITableViewCell else {
+            fatalError("Dequeueing UITableViewCell failed")
+        }
+
+        cell.textLabel?.text = "cell"
+        return cell
+    }
+
+}
